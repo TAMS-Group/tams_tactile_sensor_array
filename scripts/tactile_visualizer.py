88863d479d2cab74313db86f8d694a0b8048985e
Niklas Fiedler <git@nfiedler.de>
2021-01-12 16:12:41 +0100

    working version

---
+++ scripts/tactile_visualizer.py
@@ -0,0 +1,58 @@
+#! /usr/bin/env python3
+import numpy as np
+import cv2
+import struct
+import rospy
+import math
+from tams_tactile_sensor_array.msg import TactileSensorArrayData
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+from typing import Dict, List
+import cv2
+
+class SensorDataVisualizer:
+    def __init__(self):
+        rospy.init_node('TactileSensorVisualizer')
+
+        self.data_subscriber = rospy.Subscriber(
+            rospy.get_param('/TactileSensor/tactile/sensor_data_topic'),
+            TactileSensorArrayData,
+            self.receive_data
+        )
+
+        self.base_topic = 'tactile_viz_'
+        self.data_scale = 60
+
+        self.publishers = dict()
+        self.cv_bridge = CvBridge()
+
+
+
+        rospy.spin()
+
+    def receive_data(self, msg: TactileSensorArrayData):
+        print(msg)
+        #get publisher:
+        publisher = self.publishers.get(msg.sensor_id)
+        if not publisher:  # a new publisher has to be instantiated
+            publisher = rospy.Publisher(self.base_topic + str(msg.sensor_id), Image, queue_size=1)
+            self.publishers[msg.sensor_id] = publisher
+        data = np.array(msg.data).reshape((msg.sensor_data_width, msg.sensor_data_height)) / 4
+        data = data.astype(np.uint8)
+        img = cv2.applyColorMap(data, cv2.COLORMAP_JET)
+        img = cv2.resize(
+            img,
+            (msg.sensor_data_width * self.data_scale, msg.sensor_data_height * self.data_scale),
+            interpolation=cv2.INTER_NEAREST
+        )
+        cv2.imshow(self.base_topic + str(msg.sensor_id), img)
+        cv2.waitKey(1)
+        return
+        img_msg = self.cv_bridge.cv2_to_imgmsg(img, "bgr8")
+        img_msg.header = msg.header
+        publisher.publish(img_msg)
+
+
+
+if __name__ == '__main__':
+    SensorDataVisualizer()
---
+++ src/tams_tactile_sensor_array/tactile_sensor_array.py
@@ -0,0 +1,108 @@
+#! /usr/bin/env python3
+import serial
+import bluetooth
+from bitstring import ConstBitStream
+import numpy as np
+import cv2
+import struct
+import rospy
+import math
+from tams_tactile_sensor_array.msg import TactileSensorArrayData
+from typing import Dict, List
+
+class SensorReader:
+    def __init__(self):
+        rospy.init_node('TactileSensorArray')
+
+        self.data_publisher = rospy.Publisher(rospy.get_param('/TactileSensor/tactile/sensor_data_topic'), TactileSensorArrayData, queue_size=1)
+
+        self.use_bluetooth = rospy.get_param('/TactileSensor/tactile/use_bluetooth', False)
+
+        if self.use_bluetooth:
+            self.bt_sock = bluetooth.BluetoothSocket()
+            self.bt_sock.connect((rospy.get_param('/TactileSensor/tactile/bluetooth_device_address'), 1))
+        else:
+            self.serial = serial.Serial(rospy.get_param('/TactileSensor/tactile/serial_port'), rospy.get_param('/TactileSensor/tactile/serial_baud'))
+
+        self.sensors = dict()
+        for sensor in rospy.get_param('/TactileSensor/tactile/sensors'):
+            s = Sensor.from_dict(sensor)
+            self.sensors[s.id] = s
+
+        # wait until one transmission is completed.
+        self.wait_until_end()
+
+        while True:
+            sensor_id, data = self.receive_one()
+            self.data_publisher.publish(self.sensors[sensor_id].generate_data_msg(data))
+
+    def receive_one(self):
+        if self.use_bluetooth:
+            d = self.bt_sock.recv(1)
+        else:
+            d = self.serial.read(1)
+        sensor_number = struct.unpack('B', d)[0]
+        return sensor_number, self.receive_sensor(sensor_number)
+
+    def receive_sensor(self, sensor_number):
+        sensor = self.sensors[sensor_number]  # type: Sensor
+        if self.use_bluetooth:
+            raw_data = self.bt_sock.recv(sensor.request_size)
+            while len(raw_data) < sensor.request_size:
+                raw_data += self.bt_sock.recv(sensor.request_size - len(raw_data))
+        else:
+            raw_data = self.serial.read(sensor.request_size)
+
+        data = []
+        stream = ConstBitStream(raw_data)
+        for x in range(sensor.width * sensor.height):
+            data.append(stream.read('uint:10'))
+        return data
+
+    def wait_until_end(self):
+        # wait until one transmission is completed.
+        checkbyte = 42
+        while checkbyte != bytes.fromhex('00'):
+            if self.use_bluetooth:
+                checkbyte = self.bt_sock.recv(1)
+            else:
+                checkbyte = self.serial.read(1)
+            print(checkbyte)
+
+
+class Sensor:
+
+    def __init__(self, sensor_id, width, height, value_size, frame, active=True):
+        self.id = sensor_id
+        self.width = width
+        self.height = height
+        self.value_size = value_size
+        self.frame = frame
+        self.request_size = int(math.ceil(5/4 * self.width * self.height) + 1)
+        self. active = active
+
+    def generate_data_msg(self, data):
+        msg = TactileSensorArrayData()
+        msg.header.stamp = rospy.Time.now()
+        msg.header.frame_id = self.frame
+        msg.data = data
+        msg.sensor_data_height = self.height
+        msg.sensor_data_width = self.width
+        msg.sensor_id = self.id
+        print(msg)
+        return msg
+
+    @staticmethod
+    def from_dict(sensor_dict: Dict):
+        return Sensor(
+            sensor_dict['id'],
+            sensor_dict['width'],
+            sensor_dict['height'],
+            sensor_dict['value_size'],
+            sensor_dict['frame'],
+            sensor_dict.get('active', True)
+        )
+
+
+if __name__ == '__main__':
+    SensorReader()
